6fa2451f59068bf60ad238678f142de90575ee1a
Emanuel San Roman <emanuel.sanroman03@gmail.com>
2025-04-16 12:48:40 -0300

    Dandole funcionalidad al lapiz

---
+++ JS/Pen.js
@@ -0,0 +1,35 @@
+class Pen {
+    constructor(posX, posY, ctx, color) {
+        this.antX = posX;
+        this.antY = posY;
+        this.posX = posX;
+        this.posY = posY;
+        this.ctx = ctx;
+        this.color = color;
+    }
+
+    moveTo(x,y) {
+        this.antX = this.posX;
+        this.antY = this.posY;
+        this.posX = x;
+        this.posY = y;
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+
+    setXY(x, y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.color;
+        this.ctx.moveTo(this.antX, this.antY);
+        this.ctx.lineTo(this.posX, this.posY);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+}
